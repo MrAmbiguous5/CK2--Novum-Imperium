31cec17eb429c1a9db2d2fda0be887301481612d
MrAmbiguous5 <everforever777@outlook.com>
2023-05-18 14:38:19 -0400

    added base hip
    ....

---
+++ common/defines/emf-plus-swmh_defines.lua
@@ -0,0 +1,18 @@
+
+-- Diplomacy
+NDefines.NDiplomacy.VASSAL_LIMIT_KING_MULT = 30.0       -- up from 20
+NDefines.NDiplomacy.VASSAL_LIMIT_EMPEROR_MULT = 40.0    -- up from 30
+NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_KING_MULT = 25.0
+NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_EMPEROR_MULT = 35.0
+NDefines.NDiplomacy.MAX_PREP_INV_TARGET_HOLDINGS = 60   -- up from 40
+
+-- Nomad
+NDefines.NNomad.MAX_POPULATION_EMPTY_HOLDING_MULTIPLIER = 775 -- < v10.6: 1375; SWMH: 1550; vanilla: 1000; now compensated for with terrain-based province modifiers; [Base population per empty holding for grazing]
+
+-- Engine
+NDefines.NEngine.MISSING_SCRIPTED_SUCCESSOR_ERROR_CUTOFF_YEAR = 993
+NDefines.NEngine.MISSING_SCRIPTED_SUCCESSOR_ERROR_CUTOFF_MONTH = 1
+NDefines.NEngine.MISSING_SCRIPTED_SUCCESSOR_ERROR_CUTOFF_DAY = 1
+
+-- AI
+NDefines.NAI.COALITION_PROVINCE_MULTIPLIER = 0.7 -- down from 1.0
