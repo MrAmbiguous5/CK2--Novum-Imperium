36ab59b7c3692dc70c5230305e41bc79d99907e2
Tiara Smith <elvenjediknight@gmail.com>
2023-05-19 08:17:42 -0600

    Delete common directory

--- common/defines/A_swmh_defines.lua
+++
@@ -1,4 +0,0 @@
--- Format for overwriting define values:
-NDefines.NNomad.PROVINCES_PER_CLAN = 2
-NDefines.NNomad.MAX_POPULATION_EMPTY_HOLDING_MULTIPLIER = 1550
-NDefines.NNomad.START_POPULATION = 0.70
